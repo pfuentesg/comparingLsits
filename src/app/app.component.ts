696bcd56e8d19afe65cc28176c46161bd7ef50ae
pfuentesg <pablofuentesg1@yahoo.es>
2018-01-07 21:40:12 +0100

    layoaut, and push ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  list2:Array<string>=["Apple", "Orange", "Banana", "Blueberry", "Cherry", "Date", "Pea", "Pear", "Olive"] 
+  list1:Array<string>=[]
+  value:String=""
+  constructor(){}
+  ngOnInit() {}
+  //this function will add a new value to list1 
+  pushTo1(){
+     console.log(this.value, "holi")   
+    this.list1.push(this.value.toString().toLowerCase().replace(/ /g,"_"));
+    this.value=""
+    }
+
 }
